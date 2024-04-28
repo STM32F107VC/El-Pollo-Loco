25f65ba617131ac0f5d14c4aac14f335946958bc
STM32F107VC <baer.sydney@outlook.com>
2024-04-28 13:28:38 +0200

    Added new classs for small chickens
    Created new class for small chickens and added at first three of them to enemies array in level1.js

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -1,16 +1,12 @@
-class Chicken extends MovableObject {
-    height = 50;
-    width = 50;
-    y = 380;
-
+class ChickenSmall extends MovableObject {
     IMAGES_WALKING = [
-        '../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        '../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        '../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
-    ];
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];    
 
     constructor() {
-        super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
         this.x = 150 + Math.random() * 500;
         this.speed = 0.15 + Math.random() * 0.25;
