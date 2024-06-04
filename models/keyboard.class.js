810f0b7b2a4b16c08b9779edef8ea90f47b0e77f
STM32F107VC <baer.sydney@outlook.com>
2024-06-04 19:52:09 +0200

    Added landscape mode and shiftet controll unit
    Added landscape mode and shifter contoll unit into canvas

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,7 +1,15 @@
 class Keyboard {
-    LEFT = false; // Move left
-    RIGHT = false; // Move right
-    SPACE = false; // Jump
-    D = false; // Throw bottle
-    P = false; // Full screen
+    /**
+     * LEFT = Move left
+     * RIGHT = Move right
+     * SPACE = Jump
+     * D = Thorw bottle
+     * P = Full screen
+     * 
+     */
+    LEFT = false;
+    RIGHT = false;
+    SPACE = false; 
+    D = false;
+    P = false;
 }
